f47585d72ce19e652beaf11d2faa1b49e3aef92a
9098Tarun <tarunpsgsits07@gmail.com>
2024-09-30 21:34:05 +0530

    iteration commit

--- Shared/login.js
+++ Shared/login.js
@@ -3,9 +3,18 @@ let popup = document.getElementById("popup");
 let Timeout;
 function openPopup(){
     popup.classList.add("openPopup");
-    Timeout = setTimeout(closePopup , 3000);
+    // Timeout = setTimeout(closePopup , 3000);
     // btn.classList.add("btn btn-warning mt-3 w-25");
 }
 function closePopup(){
     popup.classList.remove("openPopup");
 }
+// let popup = document.getElementById("popup");
+// let timeout;
+// function openPopup(){
+//     popup.classList.add("openPopup");
+//     // timeout = setTimeout(closePopup,3000);
+// }
+// function closePopup(){
+//     popup.classList.remove("openPopup");
+// }
